9fea4f2fd0c6a342cb61abf9d33bdb1bf6ae093c
Tahmid Ferdous <tahmid.tf1@gmail.com>
2026-01-12 18:57:39 +0600

    problem solved : 1066 - Even, Odd, Positive and Negative

--- 1065.php
+++ 1065.php
@@ -1,7 +1,10 @@
 <?php
 
-$numbers     = [];
-$_even_count = 0;
+$numbers         = [];
+$_even_count     = 0;
+$_odd_count      = 0;
+$_positive_count = 0;
+$_negative_count = 0;
 
 for ($i = 0; $i < 5; $i++) {
     $numbers[$i] = trim(fgets(STDIN));
@@ -10,7 +13,20 @@ for ($i = 0; $i < 5; $i++) {
 for ($i = 0; $i < count($numbers); $i++) {
     if ($numbers[$i] % 2 == 0) {
         $_even_count++;
+    } else if ($numbers[$i] % 2 != 0) {
+        $_odd_count++;
     }
 }
 
-echo $_even_count . " valores pares" . PHP_EOL;
+for ($i = 0; $i < count($numbers); $i++) {
+    if ($numbers[$i] > 0) {
+        $_positive_count++;
+    } else if ($numbers[$i] < 0) {
+        $_negative_count++;
+    }
+}
+
+echo $_even_count . " valor(es) par(es)" . PHP_EOL;
+echo $_odd_count . " valor(es) impar(es)" . PHP_EOL;
+echo $_positive_count . " valor(es) positivo(s)" . PHP_EOL;
+echo $_negative_count . " valor(es) negativo(s)" . PHP_EOL;
