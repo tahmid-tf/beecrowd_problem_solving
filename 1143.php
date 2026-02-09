efa94ff44ae2c0b81e0756c876a5e05ea337703c
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2026-02-09 17:30:30 +0600

    1143 - Squared and Cubic

---
+++ 1143.php
@@ -0,0 +1,18 @@
+<?php
+
+$_array_data = [];
+$_input      = intval(trim(fgets(STDIN)));
+$_limit      = $_input * 4;
+$_number     = 1;
+
+for ($i = 1; $i <= $_limit; $i++) {
+    if ($i % 4 == 0) {
+        [$el1, $el2, $el3] = [pow($_number, 1), pow($_number, 2), pow($_number, 3)];
+        array_push($_array_data, [$el1, $el2, $el3]);
+        $_number++;
+    }
+}
+
+foreach ($_array_data as $data) {
+    echo implode(' ', $data) . "\n";
+}
