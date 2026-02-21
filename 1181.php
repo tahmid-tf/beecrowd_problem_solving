9fcba95b702247d72ee5109db4b72d9b3ea0543e
Tahmid Ferdous <tahmid.tf1@gmail.com>
2026-02-21 15:07:57 +0600

    1181 - Line in Array

---
+++ 1181.php
@@ -0,0 +1,23 @@
+<?php
+
+$_line      = intval(trim(fgets(STDIN)));
+$_operation = trim(fgets(STDIN));
+
+$M = [];
+
+for ($i = 0; $i < 12; $i++) {
+    $_temp_array = [];
+    for ($j = 0; $j < 12; $j++) {
+        $_temp_array[$j] = floatval(trim(fgets(STDIN)));
+    }
+    $M[$i] = $_temp_array;
+}
+
+$_array_sum = array_sum($M[$_line]);
+
+if ($_operation == 'S') {
+    printf("%.1f\n", $_array_sum);
+} else {
+    $_average = $_array_sum / 12.0;
+    printf("%.1f\n", $_average);
+}
